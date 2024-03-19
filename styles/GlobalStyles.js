b4fc5057ba35683ae4f1b0119bd22e927a0c393e
Mariam <mariam.mantidze1@gmail.com>
2024-03-19 20:02:11 +0400

    style cards

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: "Space Grotesk", sans-serif;
+    min-height: 100dvh;
+    display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+
+
+}
+
+button {
+    font-family: "Space Grotesk", sans-serif;
+}
+
+form, input {
+    font-family: "Space Grotesk", sans-serif;
+}
+`;
